57984a4133920f299f18ffa98110c078b26e1367
David Vizena <davidvizena@gmail.com>
2023-03-27 12:55:11 -0500

    init setup and push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, Box } from '@chakra-ui/react';
+
+const App = () => {
+	return (
+		<Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+			<Container maxW='3xl' centerContent>
+				<h1>My App</h1>
+			</Container>
+		</Box>
+	);
+};
+
+export default App;
